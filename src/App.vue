39aa5328ed2e4b3869b61378ba4110c4906b5d34
yangxiao <yangxiao0303@gmail.com>
2023-11-06 21:28:15 +0800

    17_自定义指令_局部指令

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <template>
-  <div class="container">
-    <!-- Search -->
-    <Search/>
-    <!-- List -->
-    <List/>
-
-
+  <div class="box">
+    <!-- 内置指令 dircetives -->
+    <h2 v-text="sum"></h2>
+    <!-- 自定义指令 数字10倍放大 -->
+    <h2 v-big="sum">哈哈</h2>
+    <!-- 自定义指令 beauty 改变样式 -->
+    <span v-beauty="name"></span>
   </div>
 </template>
 
 <script>
-  import Search from './components/Search.vue';
-  import List from './components/List.vue';
-  export default {
-    name:'App',
-    components:{Search,List}
+export default {
+  name: "App",
+  data() {
+    return {
+      sum: 123,
+      name: "高圆圆",
+    };
+  },
+  directives:{
+    big(element,{value}){
+      element.innerText += value*10;
+    },
+    beauty(element,{value}){
+      element.innerText = value;
+      element.style.backgroundColor = '#dd302d';
+      element.style.color = 'aqua';
+    }
   }
+};
 </script>
 
-<style>
-  
+<style scoped>
+.box{
+  background-color: #ddd;
+  border-radius: 10px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px;
+}
 </style>
