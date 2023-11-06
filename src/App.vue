4ad9e73f0a785e8e185c21148187ef8f17a4b255
yangxiao <yangxiao0303@gmail.com>
2023-11-07 00:39:44 +0800

    19_插槽_默认插槽

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,45 @@
 <template>
   <div class="box">
-    <!-- 内置指令 dircetives -->
-    <h2 v-text="sum"></h2>
-    <!-- 自定义指令 数字10倍放大 -->
-    <h2 v-big="sum">哈哈</h2>
-    <!-- 自定义指令 beauty 改变样式 -->
-    <!-- <span v-beauty="name"></span> -->
-    <Child/>
+    <Category title="游戏"> 
+      <ul>
+        <li v-for="game in games" :key="game.id">{{ game.name }}</li>
+      </ul>
+    </Category>
+    <Category title="美食"> 
+      <img :src="food" alt="好吃的">
+    </Category>
+    <Category title="影视"> 
+      <video :src="movie" controls></video>
+    </Category>
   </div>
 </template>
 
 <script>
-import Child from './components/Child.vue';
+import Category from "./components/Category.vue";
 export default {
   name: "App",
+  components: { Category },
   data() {
     return {
-      sum: 123,
+      games: [
+        { id: "asdtrd1", name: "英雄联盟" },
+        { id: "asdtrd2", name: "原神" },
+        { id: "asdtrd3", name: "红色警戒" },
+        { id: "asdtrd4", name: "斗罗大陆" },
+      ],
+      food: "http://49.232.112.44/images/food.jpg",
+      movie: "http://49.232.112.44/video/movie.mp4",
     };
   },
-  components:{Child},
-  directives:{
-    big(element,{value}){
-      element.innerText += value*10;
-    },
-    
-  }
 };
 </script>
 
 <style>
 .box{
   background-color: #ddd;
-  border-radius: 10px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px;
+  display: flex;
+  justify-content: space-evenly;
+  padding: 10px;
+  border-radius: 5px;
 }
 </style>
---
+++ src/components/Category.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="category">
+    <h3>今日{{ title }}推荐</h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+.category{
+  width: 300px;
+  height: 400px;
+  background-color: lightblue;
+  padding: 10px;
+  border: 1px double black;
+  border-radius: 5px;
+  box-shadow: 0 0 10px;
+}
+
+h3 {
+  background-color: orange;
+  font-size: 20px;
+  text-align: center;
+}
+
+img {
+  width: 100%;
+}
+
+video {
+  width: 100%;
+}
+</style>
