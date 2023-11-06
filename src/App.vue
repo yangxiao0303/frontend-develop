a3aee01308f7d8284dee9fa8b26b685bd56f3066
yangxiao <yangxiao0303@gmail.com>
2023-11-06 11:31:01 +0800

    15_Search静态页面

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,23 @@
 <template>
-  <div class="todo-container">
-    <div class="todo-wrap">
-      <Header @add-todo="addTodo" />
-      <List
-        :todos="todos"
-        :deleltTodo="deleteTodo"
-        :checkedTodo="checkedTodo"
-      />
-      <Footer
-        v-if="todos.length"
-        :todos="todos"
-        :batchChecked="batchChecked"
-        :clearCheckedTodos="clearCheckedTodos"
-      />
-    </div>
+  <div class="container">
+    <!-- Search -->
+    <Search/>
+    <!-- List -->
+    <List/>
+
+
   </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import List from "./components/List.vue";
-import Footer from "./components/Footer.vue";
-
-export default {
-  name: "App",
-  components: { Header, List, Footer },
-  data() {
-    return {
-      // todos 的初始值(初次访问则为空数组)
-      todos: JSON.parse(localStorage.getItem('todos')) || [],
-    };
-  },
-  methods: {
-    addTodo(todoObj) {
-      this.todos.unshift(todoObj);
-    },
-    deleteTodo(id) {
-      this.todos = this.todos.filter((item) => item.id !== id);
-    },
-    checkedTodo(id) {
-      // 遍历,如果id一致,则修改done的值,记得要赋值!
-      this.todos.forEach((item) =>
-        item.id === id ? (item.done = !item.done) : null
-      );
-    },
-    batchChecked(value) {
-      this.todos.forEach((item) => (item.done = value));
-    },
-    clearCheckedTodos() {
-      this.todos = this.todos.filter((item) => !item.done);
-    },
-  },
-  watch: {
-    // 监视 todos 的数据变化
-    todos: {
-      // 深度监视开启, 用来追踪 todos 内部属性 'done' 的变化
-      deep: true,
-      // value 为 vue 底层传入的更新后的 todos 的值
-      handler(value) {
-        // 变化设置更新
-        localStorage.setItem('todos',JSON.stringify(value))
-      },
-    },
-  },
-};
+  import Search from './components/Search.vue';
+  import List from './components/List.vue';
+  export default {
+    name:'App',
+    components:{Search,List}
+  }
 </script>
 
 <style>
-/*base*/
-body {
-  background: #fff;
-}
-
-.btn {
-  display: inline-block;
-  padding: 4px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
-    0 1px 2px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-}
-
-.btn-danger {
-  color: #fff;
-  background-color: #da4f49;
-  border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-  color: #fff;
-  background-color: #bd362f;
-}
-
-.btn:focus {
-  outline: none;
-}
-
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
+  
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,40 +1,68 @@
 <template>
-  <ul class="todo-main">
-    <Item 
-    v-for="todo in todos" 
-    :key= "todo.id" 
-    :todoObj = "todo" 
-    :deleteTodo = "deleltTodo"
-    :checkedTodo = "checkedTodo"
-   />
-  </ul>
+      <div class="row">
+      <div class="card">
+        <a href="https://github.com/xxxxxx" target="_blank">
+          <img src="https://v2.cn.vuejs.org/images/logo.svg" style='width: 100px'/>
+        </a>
+        <p class="card-text">xxxxxx</p>
+      </div>
+      <div class="card">
+        <a href="https://github.com/xxxxxx" target="_blank">
+          <img src="https://v2.cn.vuejs.org/images/logo.svg" style='width: 100px'/>
+        </a>
+        <p class="card-text">xxxxxx</p>
+      </div>
+      <div class="card">
+        <a href="https://github.com/xxxxxx" target="_blank">
+          <img src="https://v2.cn.vuejs.org/images/logo.svg" style='width: 100px'/>
+        </a>
+        <p class="card-text">xxxxxx</p>
+      </div>
+      <div class="card">
+        <a href="https://github.com/xxxxxx" target="_blank">
+          <img src="https://v2.cn.vuejs.org/images/logo.svg" style='width: 100px'/>
+        </a>
+        <p class="card-text">xxxxxx</p>
+      </div>
+      <div class="card">
+        <a href="https://github.com/xxxxxx" target="_blank">
+          <img src="https://v2.cn.vuejs.org/images/logo.svg" style='width: 100px'/>
+        </a>
+        <p class="card-text">xxxxxx</p>
+      </div>
+    </div>
 </template>
 
 <script>
-import Item from './Item.vue';
-
   export default {
-    name:'List',
-    components:{Item},
-    props:['todos', 'deleltTodo','checkedTodo'],
+    name:'List'
   }
 </script>
 
 <style scoped>
-  /*main*/
-.todo-main {
-  margin-left: 0px;
-  border: 1px solid #ddd;
-  border-radius: 2px;
-  padding: 0px;
+  .album {
+  min-height: 50rem; /* Can be removed; just added for demo purposes */
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  background-color: #f7f7f7;
 }
 
-.todo-empty {
-  height: 40px;
-  line-height: 40px;
-  border: 1px solid #ddd;
-  border-radius: 2px;
-  padding-left: 5px;
-  margin-top: 10px;
+.card {
+  float: left;
+  width: 33.333%;
+  padding: .75rem;
+  margin-bottom: 2rem;
+  border: 1px solid #efefef;
+  text-align: center;
 }
+
+.card > img {
+  margin-bottom: .75rem;
+  border-radius: 100px;
+}
+
+.card-text {
+  font-size: 85%;
+}
+
 </style>
