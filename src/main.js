257ca4147fbb10258c1912809d5154a9afb747c4
yangxiao <yangxiao0303@gmail.com>
2023-11-07 13:11:04 +0800

    24_单独配置Vuex环境

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue"
 import App from "./App.vue"
+import store from "./store"
 
 Vue.config.productionTip = false;
 
@@ -9,4 +10,5 @@ new Vue({
   render(h) {
     return h(App)
   },
+  store,
 }).$mount('#root');
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+// 引入Vuex
+import Vuex,{Store} from 'vuex';
+// 使用插件
+Vue.use(Vuex)
+// 创建并暴露一个store实例对象
+export default new Store({
+  actions:{},
+  mutations:{},
+  state:{}
+})
